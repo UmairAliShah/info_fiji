c97387457ec995d900e7d6b6bb38b220c5cca705
UmairAliShah <umairali43230@gmail.com>
2018-02-09 19:30:41 +0500

    My Comit on 9 feb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
 
   #Mailer Configurations Code
-  config.action_mailer.default_url_options = { host: 'info-fiji.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'info-fiji.herokuapp.com', protocol: 'http' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
